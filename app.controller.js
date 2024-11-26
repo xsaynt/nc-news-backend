91ebb458de98cf07424df3d53176e3ca6c973bf0
xsaynt <alex-price3@hotmail.co.uk>
2024-11-26 14:27:49 +0000

    Task 5 Complete - Endpoint Added

--- app.controller.js
+++ app.controller.js
@@ -45,6 +45,7 @@ exports.getMatchingComments = (req, res, next) => {
 	const article_id = req.params.article_id;
 	articleComments(article_id)
 		.then((article) => {
+			console.log(article);
 			res.status(200).send(article);
 		})
 		.catch((err) => {
