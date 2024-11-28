11ad24d092979a59113787b7fd336abb2d35f4b0
xsaynt <alex-price3@hotmail.co.uk>
2024-11-28 16:29:51 +0000

    Task 11 - .only removed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -77,7 +77,7 @@ describe('GET /api/articles/:article_id', () => {
 	});
 });
 
-describe.only('GET /api/articles', () => {
+describe('GET /api/articles', () => {
 	test('200: Returns an array of all articles sorted in descending order without a body property', () => {
 		return request(app)
 			.get('/api/articles')
