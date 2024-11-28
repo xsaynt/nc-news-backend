d322b9d8c056c8c789905bb96f41c559b7fe7165
xsaynt <alex-price3@hotmail.co.uk>
2024-11-28 14:47:44 +0000

    Suptest and Render Prep

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
-const app = require('./app.js');
+const app = require('../be-nc-news/__app__/app');
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
