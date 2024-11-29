0aa012a356017a1913d9364170c11f45e4b35495
xsaynt <alex-price3@hotmail.co.uk>
2024-11-29 11:47:19 +0000

    Task 12 Complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -41,7 +41,7 @@ describe('GET /api/topics', () => {
 	});
 });
 
-describe.only('GET /api/articles/:article_id', () => {
+describe('GET /api/articles/:article_id', () => {
 	test('200: Responds with an article of the chosen ID', () => {
 		return request(app)
 			.get('/api/articles/3')
